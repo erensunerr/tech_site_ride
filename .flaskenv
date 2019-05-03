6db940b0193372dfd391ee924a3a61010ef2bf25
Eren Suner <gerensuner@gmail.com>
2019-05-03 14:16:11 +0000

    init

---
+++ .flaskenv
@@ -0,0 +1,1 @@
+FLASK_APP=tech_site_ride.py
---
+++ app/routes.py
@@ -0,0 +1,21 @@
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    pass #TODO: create template for index page
+
+@app.route('/products')
+def products():
+    pass #TODO: code this
+
+@app.route('/legal')
+def legal():
+    pass #TODO: code this
+
+@app.route('/buy')
+def buy():
+    pass #TODO: code this
+
+#GENERAL TODO: Code a rest api to retrieve reservation info, create a db for reservation info.
+
